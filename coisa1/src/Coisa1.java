a84bafabf77eda49433e2da28ac4641b2bd87f8d
Carlos Correia <carlos.correia@sistemafiep.org.br>
2026-06-03 19:53:46 -0300

    aula 03/06

--- coisa1/src/Coisa1.java
+++ coisa1/src/Coisa1.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Coisa1 {
     public static void main(String[] args) {
         int idade;
         String nome;
