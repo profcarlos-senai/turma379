c59140e536ff43528813019d48497b3e8003903b
Carlos Correia <carlos.correia@sistemafiep.org.br>
2026-06-02 21:49:34 -0300

    aula 02/06

---
+++ coisa1/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int idade;
+        String nome;
+        char niver;
+
+        // pra ler do teclado
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Digite seu nome: ");
+        nome = teclado.nextLine();
+        System.out.print("Digite sua idade: ");
+        idade = teclado.nextInt();
+        System.out.print("Já fez aniversário (S/N)? ");
+        niver = teclado.next().charAt(0);
+
+        // adivinha o ano de nascimento
+        int ano = 2026-idade;
+        if(niver == 'N' || niver == 'n'){
+            ano--;
+        }
+        System.out.println(nome+", você nasceu em "+ano);
+    }
+}
